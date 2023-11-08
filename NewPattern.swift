95df735c4ae40184dd9d6f2581a63307cab3a9ef
Sam Cain <samuelrcain@gmail.com>
2023-11-08 16:10:28 -0600

    Large functional and cosmetic updates

--- NewPattern.swift
+++ NewPattern.swift
@@ -8,33 +8,66 @@
 import SwiftUI
 
 struct PatternEntry: View {
+    
     @EnvironmentObject var model: Section
     
     var body: some View {
         HStack {
+            Spacer()
+            
             TextField("Section Name", text: $model.name)
-            TextField("Rows", value: $model.rowCount, format: .number)
+                .textFieldStyle(.roundedBorder)
+                .frame(width: 225)
+            
+            TextField("Rows", value: $model.rows, format: .number)
+                .textFieldStyle(.roundedBorder)
+            
+            Spacer()
         }
     }
 }
 
 struct CreateView: View {
     
-    @StateObject private var pattern = Pattern()
+    @State private var rows: Int
+    @StateObject private var pattern: Pattern
+    
+    init(pattern: Pattern = Pattern()) {
+        self._rows = .init(initialValue: 1)
+        self._pattern = .init(wrappedValue: pattern)
+    }
     
     var body: some View {
+        
         NavigationView {
+            
             VStack {
-                ForEach(pattern.sections) { sec in
-                    PatternEntry().environmentObject(sec)
+                
+                ForEach(pattern.sections) { section in
+                    
+                    HStack {
+                        PatternEntry().environmentObject(section)
+                        Button("Remove", action: {
+                            pattern.sections.removeAll { sec in
+                                sec.id == section.id
+                            }
+                        })
+                        .foregroundStyle(.red)
+                    }
+                    
                 }
+                
                 Button("Add", action: {
                     pattern.sections.append(Section())
-                    
-                })
-                NavigationLink("Create") {
-                    CounterView(pattern: pattern)
-                }
+                }).padding(.top)
+                
+                Spacer()
+                
+                NavigationLink(
+                    destination: { CounterView(pattern: pattern) },
+                    label: { Text("Create Pattern") }
+                )
+                .disabled( !pattern.sections.contains(where: Section.IsInitialized) )
             }
         }
     }
@@ -42,5 +75,5 @@ struct CreateView: View {
 }
 
 #Preview {
-    CreateView()
+    CreateView(pattern: Pattern.DefaultPattern())
 }
